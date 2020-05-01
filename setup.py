3a4507172bed4b57f879bc6b13a1c769f0eaaea7
ims <ims21@users.sourceforge.net>
2020-05-01 10:09:55 +0200

    remove whitespaces

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setup_translate
 
 pkg = 'Extensions.XPower'
 setup (name = 'enigma2-plugin-extensions-xpower',
-       version = '1.59',
-       description = 'remote PC power management for Win and linux',
-       packages = [pkg],
-       package_dir = {pkg: 'plugin'},
-       package_data = {pkg: ['*.png', '*.xml', '*/*.png', 'locale/*.pot', 'locale/*/LC_MESSAGES/*.mo']},
-       cmdclass = setup_translate.cmdclass, # for translation
-      )
+	version = '1.59',
+	description = 'remote PC power management for Win and linux',
+	packages = [pkg],
+	package_dir = {pkg: 'plugin'},
+	package_data = {pkg: ['*.png', '*.xml', '*/*.png', 'locale/*.pot', 'locale/*/LC_MESSAGES/*.mo']},
+	cmdclass = setup_translate.cmdclass, # for translation
+	)
