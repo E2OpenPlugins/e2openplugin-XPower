0aa7316d65fd40ff2caadf8529e2b0caa1d97fd6
persianpros <persianpros@yahoo.com>
2021-04-14 15:24:27 +0430

    PEP8 double aggressive E401

--- plugin/ui.py
+++ plugin/ui.py
@@ -432,7 +432,11 @@ class xpower(Screen, HelpableScreen):
 	# p is ( system, ip, user, passw, mac )
 
 	def GetIPsFromNetworkInterfaces(self):
-		import socket, fcntl, struct, array, sys
+		import socket
+		import fcntl
+		import struct
+		import array
+		import sys
 		is_64bits = sys.maxsize > 2**32
 		struct_size = 40 if is_64bits else 32
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
